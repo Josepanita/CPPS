d671d4f3adf6336a94dfa3b9094ac769c7ccfe40
Jose Gomez <jose.panita@hotmail.com>
2012-08-10 16:14:08 -0430

    Añadida la funcion de consola interactiva

--- main.cpp
+++ main.cpp
@@ -2,22 +2,58 @@
 #include <stdlib.h>
 #include "stdlib.h"
 #include <fstream>
+#include <cstring>
 
 using namespace std;
 using namespace dscript;
 
-int main(int argc,char* argv[])
+int main(int argc, char const *argv[])
 {	
     context ctx;
     ctx.enable_logging(&cout);
     link_stdlib(ctx);
 
     if(argc > 1) {
-      ctx.exec(argv[1]);
+        cout<<argv[1]<<endl;
+      if( strcmp(argv[1], "-i") == 0){
+        string input, aux;
+
+        cout<<"CPPS Modo interactivo:"<<endl;
+        
+        do{
+            cout << "cpps> ";
+            getline(cin, input);
+
+            if(input=="exit" || input=="salir"){
+                cout<<"Saliendo del modo interactivo"<<endl;
+                break;
+            }else{
+                if(input.at(input.size()-1) != ';'){
+                    do{
+                        cout << "    > ";
+                        getline(cin, aux);
+                        input = input + aux;
+                    }while(aux.at(aux.size()-1) != ';');
+                    
+                }
+
+                input = ctx.trim(input);
+                ctx.eval(input);
+                cout<<endl;
+                
+            }
+
+        }while(1);
+
+        }else{
+            ctx.exec(argv[1]);
+        }
+
+
     }else{
-      printf("Error, argumentos invalidos\n"
-        "   cpps <archivo>... [- <arg1>...]   : ejecuta un programa\n");
-         exit(1);
+        printf("Error, argumentos invalidos\n"
+            "   cpps <archivo>... [- <arg1>...]   : ejecuta un programa\n");
+        exit(1);
     }
 
     return 0;
